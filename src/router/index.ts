249003b8fa9ce7f3cf7b76d1d399b5e7bd623fd3
Fer Mendoza <fernandoza@gmail.com>
2024-05-06 22:47:22 -0700

    add site map links and Gallery component template

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Gallery from '@/components/Gallery.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Gallery
+    },
+    {
+      path: '/our-story',
+      name: 'our-story',
+      component: HomeView
+    },
+    {
+      path: '/registry',
+      name: 'registry',
+      component: HomeView
+    },
+    {
+      path: '/travel-faqs',
+      name: 'travel-faqs',
+      component: HomeView
+    },
+    {
+      path: '/schedule',
+      name: 'schedule',
+      component: HomeView
+    },
+    {
+      path: '/rsvp',
+      name: 'rsvp',
       component: HomeView
     },
     {
