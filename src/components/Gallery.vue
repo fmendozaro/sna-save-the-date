aafd49395ab8422e18ebbcd63611d5d0af0e5d3d
Fer Mendoza <fernandoza@gmail.com>
2024-05-07 22:56:55 -0700

    feat: add lightbox and prep media files

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,17 +1,75 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script>
+import { ref, defineComponent } from 'vue'
+
+const testFolder = './media/'
+const fs = require('fs')
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach((file) => {
+    console.log(file)
+  })
+})
+
+// export default defineComponent({
+//   components: {
+//     VueEasyLightbox
+//   },
+//   setup() {
+//     const visibleRef = ref(false)
+//     const indexRef = ref(0) // default 0
+//     const imgsRef = ref([])
+//     // Img Url , string or Array of string
+//     // ImgObj { src: '', title: '', alt: '' }
+//     // 'src' is required
+//     // allow mixing
+
+//     const onShow = () => {
+//       visibleRef.value = true
+//     }
+//     const showSingle = () => {
+//       imgsRef.value = 'http://via.placeholder.com/350x150'
+//       // or
+//       // imgsRef.value  = {
+//       //   title: 'this is a placeholder',
+//       //   src: 'http://via.placeholder.com/350x150'
+//       // }
+//       onShow()
+//     }
+//     const showMultiple = () => {
+//       imgsRef.value = ['http://via.placeholder.com/350x150', 'http://via.placeholder.com/350x150']
+//       // or
+//       // imgsRef.value = [
+//       //   { title: 'test img', src: 'http://via.placeholder.com/350x150' },
+//       //   'http://via.placeholder.com/350x150'
+//       // ]
+//       indexRef.value = 0 // index of imgList
+//       onShow()
+//     }
+//     const onHide = () => (visibleRef.value = false)
+
+//     return {
+//       visibleRef,
+//       indexRef,
+//       imgsRef,
+//       showSingle,
+//       showMultiple,
+//       onHide
+//     }
+//   }
+// })
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div>
+    <button @click="showSingle">Show single picture.</button>
+    <button @click="showMultiple">Show a group of pictures.</button>
+
+    <vue-easy-lightbox
+      :visible="visibleRef"
+      :imgs="imgsRef"
+      :index="indexRef"
+      @hide="onHide"
+    ></vue-easy-lightbox>
   </div>
 </template>
 
