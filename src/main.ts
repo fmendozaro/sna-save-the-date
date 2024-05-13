e677d4fb4554c5e3dc8b4a7801ea45b1a893b482
Fer Mendoza <fernandoza@gmail.com>
2024-05-12 21:24:28 -0700

    feat: update and audit moduls

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import './assets/main.css'
 
-import App from './App.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import VueEasyLightbox from 'vue-easy-lightbox'
 import VueHead from 'vue-head'
 import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
+
+import App from './App.vue'
 import router from './router'
 
 import 'uikit/src/less/uikit.theme.less'
@@ -19,6 +21,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(VueHead)
 app.use(router)
-app.use(VueEasyLightbox)
+// app.use(VueEasyLightbox)
 
 app.mount('#app')
