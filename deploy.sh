deef007edc5adf663341af30dcf0c975e049da00
Fer Mendoza <fernandoza@gmail.com>
2024-05-27 17:59:39 -0700

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build-only 
+npm run build
 
 # navigate into the build output directory
 cd dist
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:fmendozaro/sna-save-the-date.git main:gh-pages
 
 cd -
