6bfdcae15d05191d32958d223d9958323579d9fd
Fer Mendoza <fernandoza@gmail.com>
2024-05-27 18:14:17 -0700

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue(), vueJsx(), nightwatchPlugin(), VueDevTools()],
   resolve: {
     alias: {
