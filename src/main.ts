01e989f8c0a03c6e3ce5a13f66d91f55f992f4ad
Fer Mendoza <fernandoza@gmail.com>
2024-05-28 21:31:55 -0700

    feat: add firebase

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,24 @@ import router from './router'
 
 import 'uikit/src/less/uikit.theme.less'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAWT0m0avwtjYTMQFhXhAMPdl1wAG0gLTI',
+  authDomain: 'save-the-date-61b09.firebaseapp.com',
+  projectId: 'save-the-date-61b09',
+  storageBucket: 'save-the-date-61b09.appspot.com',
+  messagingSenderId: '251867222036',
+  appId: '1:251867222036:web:9aed8b0a8c6ff246e25701'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
 // loads the Icon plugin
 UIkit.use(Icons)
 
