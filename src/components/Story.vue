0ad18258a9f636c3e6e14932b7742f5308167d55
Fer Mendoza <fernandoza@gmail.com>
2024-06-29 10:15:02 -0700

    fix mobile

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -7,7 +7,7 @@
       <span>Our Story</span>
     </h1>
     <div
-      class="uk-responsive-width uk-padding-large uk-overflow-hidden uk-grid-collapse uk-visible@l uk-child-width-1-2 uk-position-relative"
+      class="uk-responsive-width uk-padding-large uk-overflow-hidden uk-grid-collapse uk-child-width-1-2 uk-position-relative"
       uk-grid
     >
       <div class="garis-vertical"></div>
@@ -222,87 +222,8 @@
         </div>
       </div>
     </div>
-
-    <div
-      class="uk-responsive-width uk-padding-large uk-overflow-hidden uk-grid-collapse uk-hidden@l uk-position-relative"
-      uk-grid
-    >
-      <!-- <div class="garis-vertical"></div> -->
-      <div
-        class="uk-padding-large uk-width-expand"
-        uk-scrollspy="cls: uk-animation-slide-left-medium; repeat: true"
-      >
-        <img src="/story-1.jpeg" alt="Story 1 First Met" />
-      </div>
-      <div class="uk-padding-medium">
-        <div class="uk-card uk-card-default uk-card-hover">
-          <!-- <div class="bulet-kanan"></div> -->
-          <div class="uk-card-header uk-card-primary">
-            <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom uk-text-bold">3 September 2016</h3>
-              </div>
-            </div>
-          </div>
-          <div class="uk-card-body">
-            <p>Adventures in the big island</p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="uk-padding-large uk-width-expand"
-        uk-scrollspy="cls: uk-animation-slide-right-medium; repeat: true"
-      >
-        <img src="/story-2.jpeg" alt="Story 2 First Dated" />
-      </div>
-      <div class="uk-padding-medium">
-        <div class="uk-card uk-card-default uk-card-hover">
-          <!-- <div class="bulet-kiri"></div> -->
-          <div class="uk-card-header uk-card-primary">
-            <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom uk-text-bold">26 Mei 2017</h3>
-              </div>
-            </div>
-          </div>
-          <div class="uk-card-body">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quo, id nostrum quam
-              provident perspiciatis neque numquam commodi minima, aut soluta nulla, cum et eum ad
-              veritatis veniam, dolore recusandae?.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="uk-padding-large uk-width-expand"
-        uk-scrollspy="cls: uk-animation-slide-left-medium; repeat: true"
-      >
-        <img src="/story-3.jpeg" alt="Story 3 Khitbah" />
-      </div>
-      <div class="uk-padding-medium">
-        <div class="uk-card uk-card-default uk-card-hover">
-          <!-- <div class="bulet-kiri"></div> -->
-          <div class="uk-card-header uk-card-primary">
-            <div class="uk-grid-small uk-flex-middle" uk-grid>
-              <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom uk-text-bold">
-                  21 September 2017
-                </h3>
-              </div>
-            </div>
-          </div>
-          <div class="uk-card-body">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quo, id nostrum quam
-              provident perspiciatis neque numquam commodi minima, aut soluta nulla, cum et eum ad
-              veritatis veniam, dolore recusandae?.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
+  <!-- end of desktop -->
 </template>
 
 <script>
